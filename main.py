caeb8415b7f519860a3e1f031b7ca060c8f6e310
unknown <gamaizingg@gmail.com>
2024-03-10 13:49:22 +0300

    Чтение данных на сервере

--- main.py
+++ main.py
@@ -106,5 +106,19 @@ server_address = ('127.0.0.1', 8000)
 server_socket.bind(server_address)
 server_socket.listen()
 
-connection, client_address = server_socket.accept()
-print(f'Получен запрос на подключение от {client_address}!')
+try:
+    connection, client_address = server_socket.accept()
+    print(f'Получен запрос на подключение от {client_address}!')
+
+    buffer = b''
+
+    while buffer[-2:] != b'\r\n':
+        data = connection.recv(2)
+        if not data:
+            break
+        else:
+            print(f'Получены данные: {data}!')
+            buffer += data
+    print(f'Все данные: {buffer}')
+finally:
+    server_socket.close()
